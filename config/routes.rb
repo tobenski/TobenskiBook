d0ebcc5d495288d64044376010211f53761b9c44
Knud Rishøj <tirdyr@tobenski.dk>
2019-01-14 23:42:35 +0100

    added users and devise gem for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hej'
+  #root 'static_pages#home'
+
+  authenticated :user do
+    root to: 'static_pages#home', as: :authenticated_root
+    get 'static_pages/home', as: 'home'
+  end
+  root to: redirect('/users/sign_in')
 end
